a5e4699640bb934c12ba464c36deaa3b879e3a10
huluo <huyunluo@gmail.com>
2017-04-11 15:48:09 +0800

    feat: 在thymeleaf的基础上完成了表单的回显

--- src/main/java/org/huluo/thymeleaf2/spring3/controller/HelloController.java
+++ src/main/java/org/huluo/thymeleaf2/spring3/controller/HelloController.java
@@ -1,8 +1,13 @@
 package org.huluo.thymeleaf2.spring3.controller;
 
+import org.huluo.thymeleaf2.spring3.entity.Student;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
 
 @Controller
 public class HelloController {
@@ -10,6 +15,24 @@ public class HelloController {
     @RequestMapping("/aaa")
     public String sayHello() {
         return "success";
+    }
+
+
+
+    @RequestMapping(value = "/student" ,method = RequestMethod.GET)
+    public String getSayWorld(Model model ) {
+        Student student = new Student();
+        model.addAttribute("student", student);
+        return "result";
+    }
+
+    @RequestMapping(value = "/student", method = RequestMethod.POST)
+    public String postSayWorld(@Valid Student student, Model model) {
+
+        Student s = new Student();
+        s.name = "zhangsan";
+        model.addAttribute("student", s);
+        return "result";
     }
 
 }
---
+++ src/main/java/org/huluo/thymeleaf2/spring3/entity/Student.java
@@ -0,0 +1,11 @@
+package org.huluo.thymeleaf2.spring3.entity;
+
+public class Student {
+    public String name;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
